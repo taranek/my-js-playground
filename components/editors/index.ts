928ecec722f2263b7265410bf781b363847df633
taranek <t.taranek27@gmail.com>
2022-06-26 18:48:37 +0200

    chore(): remove prisma stale code

---
+++ components/editors/index.ts
@@ -0,0 +1,9 @@
+import BaseEditor from "./BaseEditor/BaseEditor";
+import EditorErrorBoundary from "./EditorErrorBoundary/EditorErrorBoundary";
+import MainEditor from "./MainEditor/MainEditor";
+
+export {
+  BaseEditor,
+  EditorErrorBoundary,
+  MainEditor
+}
